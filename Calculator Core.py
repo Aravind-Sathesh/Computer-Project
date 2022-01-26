db7ad3e72d64b9e1654e118bb35a05319918c4f2
Cowpoxman <98141133+Cowpoxman@users.noreply.github.com>
2022-01-26 22:35:31 +0530

    Update Calculator Core.py
    did a bit of testing and went thru the code ... apart from those mistakes with log and sort , I can't find anything wrong but y'all have to stress test this

--- Calculator Core.py
+++ Calculator Core.py
@@ -1,29 +1,54 @@
 try:
     from sympy import *
 except:
-    import os 
+    import os
     os.system('pip install sympy')
+    # auto installs sympy if unavailable
     from sympy import *
 
 def calculator(flag):
-    # if flag is a string , it means to take value input from console
+    global roots
+    roots = ''
+
+    # if flag is a 'console' , it means to take value input from console
     # if flag is a value , the flag is a result from a previous calculation which is to be used now
-    print('')
+    choice = int(input('''Do you want to evaluate 
+1) An Expression
+2) An Equation
+
+Enter choice 1 or 2 :'''))
+    print(choice )
+
+
+    if choice == 1:
+        print('You have chosen An Expression : ')
+    elif choice == 2:
+        print('You have chosen An Equation')
+    else:
+        print('Invalid Choice ')
+        calculator(flag)
     if flag != 'console':
-        print('\nres = '+ str(flag))
-        print('use "res" in the following expression for above value')
-    var = input('enter the variable to be solved [enter "None" if expression ] : ')
-    exp = str(input('enter expression or equation in one variable = 0 : '))
+        print('\n     res = '+ str(flag))
+        print('     NOTE : use "res" in the following expression for above value\n')
+    var = ''
+
+    if choice == 2:
+        var = input('enter the variable to be solved : ')
+        exp = str(input(('enter equation in one variable = 0 [Note "I" may be taken as sqrt(-1)]: ')))
+    else:
+        exp = str(input('enter expression to be solved : '))
     print('')
     if ('res' in exp):
-
-        exp= exp[:exp.index('res')] + str(flag) + exp[exp.index('res')+3:]
+        # replace res with its actual value in expression
+        # exp= exp[:exp.index('res')] + '(' + str(flag) +')' + exp[exp.index('res')+3:]
+        exp = exp.replace('res','('+str(flag)+')')
 
     global result
     result = 0.0
+
     try:
 
-        if var != 'None':
+        if choice == 2:
 
             x= symbols(var)
             print("equation : {} = 0".format(exp))
@@ -31,33 +56,56 @@ def calculator(flag):
             print('roots : ')
 
             for i in range(len(roots)):
+                ##
+                # if var != 'I':
+                #     roots[i] = str(roots[i]).replace('*I','j')
+                # #    
+                # print(roots)
                 print(str(var) + str(i + 1) + '= ' + str(eval(str(roots[i]))))
-
-            result = eval(str(roots[0]))
+            ##
+            if len(roots) != 0:
+                result = str(eval(str(roots[0])))
+            else:
+                print('Either no roots exist , or equation cannot be handled by SymPy')
+                result = '0.0'
 
         else:
             print('expression = ' + exp)
-            print('result : ' + str(eval(exp)))
-            result = eval(exp)
+
+            result = str(eval(exp))
+            print('result : ' + result)
 
         ans = input('\nSend output to calculator ? [yes/no] : ').lower()
 
         if ans=='yes' :
+
+            if len(roots) >= 2:
+                ch = int(input('Enter which value you want to export [1,2...] :'))
+                if 0 < ch <= len(roots):
+                    result = roots[ch-1]
+                else:
+                    print('Exporting default')
+
             calculator(result)
 
         else:
             ans2 = input('Send output to converter ? [yes/no] : ').lower()
             if ans2 == 'yes':
+                if len(roots) >= 2:
+                    ch = int(input('Enter which value you want to export [1,2...] :'))
+                    if 0 < ch <= len(roots):
+                        result = roots[ch - 1]
+                    else:
+                        print('Exporting default')
                 # converter(result)
-                # once we combine programs , we'll call the converter function here 
-                pass
+
 
             else:
                 print('Thank you ! Exiting ...')
-
-    except Exception as e:
-        print('\nerror in expression due to "' + str(e) +'..."\ntry again ')
-        calculator(float(flag))
+##
+    except:
+        print('\nerror in expression...\ntry again ')
+        calculator(eval(flag))
 
 
 
